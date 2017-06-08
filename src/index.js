fab2ae1d21e9184085ff6971aaac640f28b5a51c
kevinkid135 <kevinkid228@hotmail.com>
2017-06-07 23:05:03 -0400

    Completed Tic-Tac-Toe Tutorial Includes: * Bold the currently-selected item in the move list * Rewrite Board to use two loops to make the squares instead of hardcoding them

---
+++ src/index.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+/**
+ * Created by Kevin on 06/01/2017.
+ */
+function Square(props) {
+    return (
+        <button className="square" style={props.style} onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.props.squares[i]}
+                style={this.props.style}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const board = [];
+        for (let i = 0; i < 9; i += 3) {
+            let square = [];
+            for(let j = i; j < i+3; j++){
+                square.push(this.renderSquare(j));
+            }
+            board.push(<div classname="board-row">{square}</div>);
+        }
+
+        return (
+            <div>
+                {board}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) ? false : true,
+        });
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Move #' + move :
+                'Game start';
+            const styles = (this.state.stepNumber === move) ? {fontWeight: 'bold'} : {fontWeight: 'normal'};
+            return (
+                <li key={move}>
+                    <a href="#" style={styles} onClick={() => this.jumpTo(move)}>{desc}</a>
+                </li>
+            )
+        });
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
+        }
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares [c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
